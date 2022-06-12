f7896c0d9bbd36c972fde4328bc009b3026c0780
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-06-12 13:03:25 +0200

    Update

--- app/src/main/java/irawan/electroshock/tungpat/MainActivity.java
+++ app/src/main/java/irawan/electroshock/tungpat/MainActivity.java
@@ -14,38 +14,20 @@ import irawan.electroshock.tungpat.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     Random random = new Random();
     ArrayList<Integer> answer = new ArrayList<>();
     int locationOfCorrectAnswer;
+    TextView question, correct, timer, points;
+    Button button0, button1, button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        TextView question = binding.sumTextView;
-        Button button0 = binding.button0;
-        Button button1 = binding.button1;
-        Button button2 = binding.button2;
-        Button button3 = binding.button3;
-        TextView correct = binding.correctTextView;
-        TextView timer = binding.timerTextView;
-        TextView points = binding.pointsTextView2;
-
-
-        int a = random.nextInt(101);
-        int b = random.nextInt(101);
-        randomAnswerGenerator(a,b);
-
-        question.setVisibility(View.INVISIBLE);
-        button0.setVisibility(View.INVISIBLE);
-        button1.setVisibility(View.INVISIBLE);
-        button2.setVisibility(View.INVISIBLE);
-        button3.setVisibility(View.INVISIBLE);
-        correct.setVisibility(View.INVISIBLE);
-        timer.setVisibility(View.INVISIBLE);
-        points.setVisibility(View.INVISIBLE);
+        initWidgets(binding);
+        questionGenerator();
 
         binding.startButton.setOnClickListener(view -> {
             binding.startButton.setVisibility(View.INVISIBLE);
@@ -56,18 +38,42 @@ public class MainActivity extends AppCompatActivity {
             button1.setVisibility(View.VISIBLE);
             button2.setVisibility(View.VISIBLE);
             button3.setVisibility(View.VISIBLE);
-            question.setText(getString(R.string.plus, a, b));
+
             button0.setText(String.valueOf(answer.get(0)));
             button1.setText(String.valueOf(answer.get(1)));
             button2.setText(String.valueOf(answer.get(2)));
             button3.setText(String.valueOf(answer.get(3)));
         });
     }
+    private void initWidgets(ActivityMainBinding binding){
+        question = binding.sumTextView;
+        button0 = binding.button0;
+        button1 = binding.button1;
+        button2 = binding.button2;
+        button3 = binding.button3;
+        correct = binding.correctTextView;
+        timer = binding.timerTextView;
+        points = binding.pointsTextView2;
 
-    private void randomAnswerGenerator(int a, int b){
+        question.setVisibility(View.INVISIBLE);
+        button0.setVisibility(View.INVISIBLE);
+        button1.setVisibility(View.INVISIBLE);
+        button2.setVisibility(View.INVISIBLE);
+        button3.setVisibility(View.INVISIBLE);
+        correct.setVisibility(View.INVISIBLE);
+        timer.setVisibility(View.INVISIBLE);
+        points.setVisibility(View.INVISIBLE);
+    }
+
+    private void questionGenerator(){
+        int a = random.nextInt(101);
+        int b = random.nextInt(101);
         int incorrectAnswer;
-
         locationOfCorrectAnswer = random.nextInt(4);
+
+        question.setText(getString(R.string.plus, a, b));
+
+        answer.clear();
         for(int i=0;i<4;i++){
             if(i == locationOfCorrectAnswer){
                 answer.add(a + b);
