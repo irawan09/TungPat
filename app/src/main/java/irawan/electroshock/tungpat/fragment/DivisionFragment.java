3991caffc67ab95a4aca62814d6203e6e0e80e96
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-06-17 10:22:11 +0200

    Update Division

--- app/src/main/java/irawan/electroshock/tungpat/fragment/DivisionFragment.java
+++ app/src/main/java/irawan/electroshock/tungpat/fragment/DivisionFragment.java
@@ -38,7 +38,7 @@ public class DivisionFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentDivisionBinding.inflate(inflater, container, false);
         return binding.getRoot();
